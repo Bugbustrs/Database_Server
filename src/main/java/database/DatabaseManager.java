08751bb1d6b3d8fe28379a32c989545707f1c639
Meluleki Dube <mellarmellah@gmail.com>
2019-08-22 12:25:57 +0200

    Fixed the task key issue

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -312,7 +312,7 @@ public class DatabaseManager {
             String user = object.getString("account_name");
             measurements.setUserName(hashUserName(user));
             measurements.setExperiment(object.getBoolean("is_experiment"));
-            if(object.has("task_key"))
+            if(object.get("task_key")!=null)
                 measurements.setTaskKey(object.getString("task_key"));
             return measurements;
         } catch (InstantiationException | IllegalAccessException e) {
